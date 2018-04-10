9e19d3eb949ceea236c189b3c36af7f9accf4d79
Arthur Vervaet <arthur.vervaet@cognitive-matchbox.com>
2018-04-10 21:31:37 +0200

    add tre structure

--- driver.py
+++ driver.py
@@ -4,69 +4,73 @@ Created on Sun Apr 01 19:02:39 2018
 
 @author: Arthur Vervaet
 """
-
-
-initialState = [1,2,5,3,4,0,6,7,8]
-goalState = [0,1,2,3,4,5,6,7,8]
+class node:
+    
+        def __init__(self,state,parent,move):
+            self.state = state 
+            self.parent = parent
+            self.move = move
+        
+        def getNeighbors(self):
+            i = self.state.index(0)
+            neighbors = []
+            
+            if i>2 :
+                neighbor = node(list(self.state),self,'Up')
+                neighbor.state[i], neighbor.state[i-3] = neighbor.state[i-3], neighbor.state[i]
+                neighbors.append(neighbor)
+            
+            if i<6:
+                neighbor = node(list(self.state),self,'Down')
+                neighbor.state[i], neighbor.state[i+3] = neighbor.state[i+3], neighbor.state[i]
+                neighbors.append(neighbor)
+                
+            if not i in [0,3,6]:
+                neighbor = node(list(self.state),self,'Left')
+                neighbor.state[i], neighbor.state[i-1] = neighbor.state[i-1], neighbor.state[i]
+                neighbors.append(neighbor)
+                
+            if not i in [2,5,8]:
+                neighbor = node(list(self.state),self,'Right')
+                neighbor.state[i], neighbor.state[i+1] = neighbor.state[i+1], neighbor.state[i]
+                neighbors.append(neighbor)
+                
+            return neighbors
+  
+initialNode = node([1,2,5,3,4,0,6,7,8],None,'Init')
+goalNode = node([0,1,2,3,4,5,6,7,8],None,'End')
 
 
 
 def bfs(initialState,goalState):
-    frontier = []
-    frontier.append(initialState)
+    frontier = [initialState]
     explored = []
     
     while frontier != []:
         
-        state = frontier[0]
+        currentNode = frontier[0]
         del frontier[0]
-        explored.append(state)
+        explored.append(currentNode)
         
-        if state == goalState:
-            print(state)
+        if currentNode.state == goalNode.state:
+            showPath(currentNode)
             return "Success"
         
-        neighbors = getNeighbors(state)
+        neighbors = currentNode.getNeighbors()
         for neigbhor in neighbors :
             if not neigbhor in explored:
                 if not neigbhor in frontier:
                     frontier.append(neigbhor)
                     
-    return "Failure"
+    return "Failure"      
 
-def getNeighbors(state):
-    i = state.index(0)
-    neighbors = []
-    print(state)
-    
-    if i>2 :
-        neighbor = list(state)
-        neighbor[i], neighbor[i-3] = neighbor[i-3], neighbor[i]
-        neighbors.append(neighbor)
-        print('Up')
- 
-    print(state)
-    
-    if i<6:
-        neighbor = list(state)
-        neighbor[i], neighbor[i+3] = neighbor[i+3], neighbor[i]
-        neighbors.append(neighbor)
-        print('Down')
-        
-    if not i in [0,3,6]:
-        neighbor = list(state)
-        neighbor[i], neighbor[i-1] = neighbor[i-1], neighbor[i]
-        neighbors.append(neighbor)
-        print('Left')
-        
-    if not i in [2,5,8]:
-        neighbor = list(state)
-        neighbor[i], neighbor[i+1] = neighbor[i+1], neighbor[i]
-        neighbors.append(neighbor)
-        print('Right')
-        
-    return neighbors
+def showPath(node):
+    path = []
+    while node != None:
+        path.append(node.move)
+        previous = node.parent
+        node = previous
+    path = path[::-1]
+    print (path)
 
-
-        
-bfs(initialState,goalState)
+bfs(initialNode,goalNode)
